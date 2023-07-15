24280ed44a478262e28aff66e3f53b67ce0d4e6a
hhaarrsshhaa <35552227+hhaarrsshhaa@users.noreply.github.com>
2023-07-15 18:46:06 +0530

    Update home.php

--- website/content/home.php
+++ website/content/home.php
@@ -1,4 +1,4 @@
-<p>This is <b>home</b> Welcome to test project -- this content is in file -- content/home.php
-This is Edureka Devops course project by SRI HARSHA </p>
-<p>making changes</p>
-</p>It is a long established fact that no one reads this and I may add anything and reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+<p><b>Welcome to test project</b></p>
+<p>This is Edureka Devops course project by SRI HARSHA </p>
+<p><b>Project SUmmary</b></p>
+</p>As soon as the developer pushes the updated code on the GIT master branch, a new test server should be provisioned with all the required software. Post this, the code should be containerized and deployed on the test server. The deployment should then be built and pushed to the prod server. All this should happen automatically and should be triggered from a push to the GitHub master branch..</p>

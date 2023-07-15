41ebf549c57cba4ac350530f870320de79722141
hhaarrsshhaa <35552227+hhaarrsshhaa@users.noreply.github.com>
2023-07-15 18:53:43 +0530

    Update home.php

--- website/content/home.php
+++ website/content/home.php
@@ -1,4 +1,4 @@
 <p><b>Welcome to test project</b></p>
 <p>This is Edureka Devops course project by SRI HARSHA </p>
-<p><b>Project SUmmary</b></p>
+<p><b>Project Summary</b></p>
 </p>As soon as the developer pushes the updated code on the GIT master branch, a new test server should be provisioned with all the required software. Post this, the code should be containerized and deployed on the test server. The deployment should then be built and pushed to the prod server. All this should happen automatically and should be triggered from a push to the GitHub master branch..</p>
